505dc517dd71d886965a1ead28d235c6207ca4f2
William Aguas <148153196+WILLIAMAGUAS@users.noreply.github.com>
2025-05-08 23:58:14 +0000

    proyecto terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [listaDeTareas, setListaDeTareas] = useState([])
+	const [nuevaTarea, setNuevaTarea] = useState("")
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+
+	function agregarTarea(evento) {
+		if (evento.key === "Enter" && nuevaTarea.trim() !== "") {
+			setListaDeTareas([...listaDeTareas, nuevaTarea.trim()])
+			setNuevaTarea("")
+		}
+	}
+
+	function eliminarTarea(indiceAEliminar) {
+		const tareasActualizadas = listaDeTareas.filter((_, indice) => indice !== indiceAEliminar)
+		setListaDeTareas(tareasActualizadas)
+
+	}
+
+	return (
+		<div className="d-flex flex-column align-items-center text-center container">
+			<h1 style={{ color: "#dc2626" }}>
+				¡¡Todolist!!
+			</h1>
+			<div className="w-100 todos-container">
+				<input type="text" className="form-control" id="todo" onChange={(evento) => { setNuevaTarea(evento.target.value) }} value={nuevaTarea} onKeyDown={(evento) => { agregarTarea(evento) }} style={{boxShadow: "0px 0px 105px 0px rgba(255,46,98,0.9)"}} />
+				{
+					listaDeTareas.map((tarea, indice) => {
+						return (
+							<div className="tarea d-flex justify-content-between align-items-center p-2 border-bottom" key={indice}>
+								<span className="text-start py-2">{tarea}</span>
+								<button className="btn btn-sm btn-danger" onClick={() => eliminarTarea(indice)}>X</button>
+							</div>
+						)
+					})
+				}
+
+				<p className="text-start form-text">
+					{listaDeTareas.length + " tareas pendientes"}
+				</p>
+
+			</div>
+
+
+
+
 		</div>
 	);
 };
